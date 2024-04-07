1643050ba1dc5d3756269e087af155a227395d3e
vedjuno236 <vedjuno236@gmail.com>
2024-04-07 15:22:56 +0700

    add payment

--- lib/app/model/payment_model.dart
+++ lib/app/model/payment_model.dart
@@ -1,5 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_admin/app/model/booking_model.dart';
+import 'package:flutter_admin/app/model/bus_model.dart';
+import 'package:flutter_admin/app/model/bustype_model.dart';
+import 'package:flutter_admin/app/model/departures_model.dart';
+import 'package:flutter_admin/app/model/passengers_model.dart';
+import 'package:flutter_admin/app/model/routes_model.dart';
+import 'package:flutter_admin/app/model/stations_model.dart';
+import 'package:flutter_admin/app/model/tickets_model.dart';
 
 class Payment {
   String id;
@@ -20,19 +27,69 @@ class Payment {
     required this.user_id,
   });
 
-Payment.fromJson(Map<String, dynamic>? json)
-    : description = json?['description'] as String? ?? '',
-      pay_date = (json?['pay_date'] as Timestamp?) ?? Timestamp.now(),
-      payment_method = json?['payment_method'] as String? ?? '',
-      total = int.parse(json?['total'] as String? ?? '0'), 
-      user_id = json?['user_id'] as String? ?? '',
-      id = json?['id'] as String? ?? '',
-      booking_id = (json?['booking_id'] as List<dynamic>?)
-              ?.map((bookingJson) {
-                print('bookingJson: $bookingJson');
-                return Booking.fromJson(bookingJson as Map<String, dynamic>);
-              })
-              .toList() ?? [];
+  Payment.fromJson(Map<String, dynamic>? json)
+      : description = json?['description'] as String? ?? '',
+        pay_date = (json?['pay_date'] as Timestamp?) ?? Timestamp.now(),
+        payment_method = json?['payment_method'] as String? ?? '',
+        total = int.parse(json?['total'] as String? ?? '0'),
+        user_id = json?['user_id'] as String? ?? '',
+        id = json?['id'] as String? ?? '',
+        booking_id =
+            (json?['booking_id'] as List<dynamic>?)?.map((bookingJson) {
+                  print('bookingJson: $bookingJson');
+                  return Booking.fromJson(bookingJson as Map<String, dynamic>);
+                }).toList() ??
+                [
+                  Booking(
+                      id:json!['id']! as String? ?? '',
+                      book_date: json['book_date']! as Timestamp,
+                      expired_time: json['expired_time']! as Timestamp,
+                    
+                      seat:json['seat'] as String? ?? '',
+                      status:json['status'] as String? ?? '',
+                  
+                      time: json['time']! as Timestamp,
+                      user_id: json['user_id'] as String? ?? '',
+                      ticket_id:  json['ticket_id'] != null
+            ? Tickets.fromJson(json['ticket_id'])
+            : Tickets(name: '', booking_price: 0, price: 0),
+                      departure_id:json['departure_id'] != null
+            ? Departures.fromJson(json['departure_id'])
+            : Departures(
+                id: '',
+                bus_id: Buses(
+                  id: '',
+                  name: '',
+                  bus_type_id: Bustype(name: ''),
+                  carnamber: '',
+                  capacity: 0,
+                  capacityVip: 0,
+                  ticketId: [],
+                ),
+                route_id: Routes(
+                  id: '',
+                  arrival_station_id: Stations(id: '', name: ''),
+                  arrival_time: Timestamp.now(),
+                  departure_station_id: Stations(id: '', name: ''),
+                  departure_time: Timestamp.now(),
+                ),
+                
+              ),
+                      passenger_id: json['passenger_id'] != null
+            ? Passengers.fromJson(json['passenger_id'])
+            : Passengers(
+                id: '',
+                name: '',
+                email: '',
+                dob: Timestamp.now(),
+                idCard: 0,
+                idCardImageUrl: '',
+                userId: '',
+                phoneNumber: '',
+                profileImageUrl: '',
+              ),
+                      )
+                ];
 
   Map<String, dynamic> toJson() {
     return {
@@ -40,101 +97,10 @@ Payment.fromJson(Map<String, dynamic>? json)
       'description': description,
       'pay_date': pay_date,
       'payment_method': payment_method,
-      'total': total.toString(), 
+      'total': total.toString(),
       'user_id': user_id,
       'id': id,
     };
   }
 }
 
-
-
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:flutter_admin/app/model/booking_model.dart';
-// import 'package:flutter_admin/app/model/departures_model.dart';
-
-// class Payment {
-//   String id;
-//   List<Booking> booking_id;
-//   String description;
-//   Timestamp payDate;
-//   String paymentMethod;
-//   int total;
-//   String userId;
-
-//   Payment({
-//     required this.id,
-//     required this.booking_id,
-//     required this.description,
-//     required this.payDate,
-//     required this.paymentMethod,
-//     required this.total,
-//     required this.userId,
-//   });
-
-//   Payment.fromJson(Map<String, dynamic> json)
-//       : id = json['id'] as String? ?? '',
-//         booking_id = (json['booking_id'] as List<dynamic>?)
-//             ?.map((bookingJson) => Booking.fromJson(bookingJson as Map<String, dynamic>))
-//             .toList() ?? [],
-//         description = json['description'] as String? ?? '',
-//         payDate = json['pay_date'] as Timestamp? ?? Timestamp.now(),
-//         paymentMethod = json['payment_method'] as String? ?? '',
-//         total = json['total'] as int? ?? 0,
-//         userId = json['user_id'] as String? ?? '';
-
-//   static Future<Departures> fromJsonWithStationNames(
-//       Map<String, dynamic> json) async {
-//     String arrivalStationName =
-//         await fetchStationName(json['route']['arrival_station_id'] as String);
-//     String departureStationName =
-//         await fetchStationName(json['route']['departure_station_id'] as String);
-
-//     return Departures.fromJson({
-//       'id': json['departure_id']['id'],
-//       'bus_id': {
-//         'id': json['departure_id']['bus_id']['id'],
-//         'name': json['departure_id']['bus_id']['name'],
-//         'bus_type_id': {'name': json['departure_id']['bus_id']['bus_type_id']['name']},
-//         'carnamber': json['departure_id']['bus_id']['carnamber'],
-//         'capacity': json['departure_id']['bus_id']['capacity'],
-//         'capacityVip': json['departure_id']['bus_id']['capacityVip'],
-//         'ticketId': json['departure_id']['bus_id']['ticketId'],
-//       },
-//       'route_id': {
-//         'id': json['departure_id']['route_id']['id'],
-//         'arrival_station_id': {
-//           'id': json['departure_id']['route_id']['arrival_station_id'],
-//           'name': arrivalStationName,
-//         },
-//         'arrival_time': json['departure_id']['route_id']['arrival_time'],
-//         'departure_station_id': {
-//           'id': json['departure_id']['route_id']['departure_station_id'],
-//           'name': departureStationName,
-//         },
-//         'departure_time': json['departure_id']['route_id']['departure_time'],
-//       },
-//     });
-//   }
-
-//   // Static method to fetch station name from Firestore
-//   static Future<String> fetchStationName(String stationId) async {
-//     DocumentSnapshot stationSnapshot = await FirebaseFirestore.instance
-//         .collection('Stations')
-//         .doc(stationId)
-//         .get();
-//     return stationSnapshot.exists ? stationSnapshot.get('name') : '';
-//   }
-
-//   Map<String, dynamic> toJson() {
-//     return {
-//       'booking_id': booking_id.map((booking) => booking.toJson()).toList(),
-//       'description': description,
-//       'pay_date': payDate,
-//       'payment_method': paymentMethod,
-//       'total': total.toString(),
-//       'user_id': userId,
-//       'id': id,
-//     };
-//   }
-// }
